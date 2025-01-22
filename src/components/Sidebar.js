b07778716a54b04e806ce394eb735d23adf147b6
rival <erodriguez2027@atmosphere.org>
2025-01-21 20:59:47 -0500

    access granted

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -106,6 +106,24 @@ function Sidebar() {
                 </div>
               </span>
             </Link>
+            <Link to={'/'} style={{ textDecoration: "none" }}>
+              <span
+                className="list-group-item list-group-item-action "
+                style={{
+                  backgroundColor: "#10002B",
+                  color: "white",
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  border:'none'
+
+                }}
+              >
+                <div style={{display:'flex', flexDirection:'row', gap:'10%'}}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+                <strong>Logout</strong>
+                </div>
+              </span>
+            </Link>
           </div>
         </div>
       </div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -28,24 +28,7 @@ function Home() {
                <div className='shadow p-3 mb-5 bg-white rounded' style={{ display: 'flex', flexDirection: 'row', width: '500px', height: '40vh', marginTop: '6%', }}></div>
                <div className='shadow p-3 mb-5 bg-white rounded' style={{ display: 'flex', flexDirection: 'row', width: '500px', height: '40vh', marginTop: '6%', }}></div>
             </div>
-            <div className="dropdown-container">
-               <button className="dropdown-toggle" onClick={toggleDropdown}>
-                  Dropdown button
-               </button>
-               {isOpen && (
-                  <ul className="dropdown-menu">
-                     <li className="dropdown-item">
-                        <a href="#action1">Action 1</a>
-                     </li>
-                     <li className="dropdown-item">
-                        <a href="#action2">Action 2</a>
-                     </li>
-                     <li className="dropdown-item">
-                        <a href="#action3">Action 3</a>
-                     </li>
-                  </ul>
-               )}
-            </div>
+            
 
 
 
